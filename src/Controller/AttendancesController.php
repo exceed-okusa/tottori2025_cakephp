670975418b8ac6b39ad0b918e95966f78efbb12a
n-okusa <n-okusa@exceed-system.co.jp>
2026-06-17 19:09:02 +0900

    １つのデータの追加を実装

--- src/Controller/AttendancesController.php
+++ src/Controller/AttendancesController.php
@@ -125,11 +125,20 @@ class AttendancesController extends BaseController
         // ここから処理を記述
         $this->logNotice($data);
 
+
+        $loginUserId = $this->request->session()->read('loginUserId');
+        $now = new FrozenTime();
         $attendance = $this->Attendances->newEntity([
             'lecture_id'        => $data['lecture_id'],
             'student_user_id'   => $data['attendance_status_list'][0]['user_id'],
             'lecture_number'    => $data['attendance_status_list'][0]['lecture_number'],
-            'attendance_status' => $data['attendance_status_list'][0]['status']
+            'attendance_status' => $data['attendance_status_list'][0]['status'],
+            'semester'          => $this->Enum->Semester->FIRST_SEMESTER->value,
+            'insert_date'       => $now,
+            'insert_user_id'    => $loginUserId,
+            'update_date'       => $now,
+            'update_user_id'    => $loginUserId,
+            'invalidation_flag' => $this->Enum->InvalidationFlag->OFF->value,
         ], ['associated'=>false]);
         $this->logNotice($attendance);
 
